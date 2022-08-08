9036a09943aec9cbf6b51c0ec886ae6c7ca49fbe
rod-cami <camilarodriguez4646@gmail>
2022-08-08 13:06:04 -0300

    Base de la tarea

---
+++ Tarea/js/app1.js
@@ -0,0 +1,19 @@
+function mensajeAlert() {
+  alert('Hola');
+}
+
+function mensajeConsole() {
+  console.log('Hola');
+}
+
+function tablas() {
+
+  console.log('Tabla del 5');
+  for (let index = 1; index <= 10; index++){
+    console.log(5*index);
+  }
+  console.log('Tabla del 6');
+  for (let index = 1; index <= 10; index++){
+    console.log(6*index);
+  }
+}
